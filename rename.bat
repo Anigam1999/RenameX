869b7252c55849380c3b6cec6185753f23077ce0
Abhishek Nigam <abhisheknigam755@gmail.com>
2023-10-25 02:22:08 +0530

    revise

--- rename.bat
+++ rename.bat
@@ -1,1 +1,3 @@
+@REM A .bat file, also known as a batch file, is a script file used in Windows operating systems to automate and streamline various tasks. These files contain a series of commands that are executed one after the other when the .bat file is run. Batch files are written in plain text and use the .bat file extension. 
+
 node ./index.js
